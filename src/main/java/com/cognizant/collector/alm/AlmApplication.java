5c7e74112f6171eca72a6c8b2071a41b1b6b5c64
138497 <subramani.murthy@cognizant.com>
2020-10-12 03:51:05 -0700

    12Oct2020

---
+++ src/main/java/com/cognizant/collector/alm/AlmApplication.java
@@ -0,0 +1,17 @@
+package com.cognizant.collector.alm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableFeignClients
+@SpringBootApplication
+@EnableScheduling
+public class AlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmApplication.class, args);
+	}
+
+}

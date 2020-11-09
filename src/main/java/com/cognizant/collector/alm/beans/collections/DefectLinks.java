a2e8d9f3d0e2c60ee338fbaa43d856defdb55080
ANPRRMLFST <ANPRRMLFST@Fastest.local>
2020-11-09 02:08:18 -0800

    Moving gitlab code to github on 11-Nov-2020

---
+++ src/main/java/com/cognizant/collector/alm/beans/collections/DefectLinks.java
@@ -0,0 +1,22 @@
+package com.cognizant.collector.alm.beans.collections;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document("source_ALMDefectLinks")
+public class DefectLinks {
+    private String owner;
+    private String secondEndpointId;
+    private String lastModified;
+    private String secondEndpointStatus;
+    private String linkType;
+    private String secondEndpointName;
+    private String creationTime;
+    private String firstEndpointId;
+    private String comment;
+    private String id;
+    private String secondEndpointType;
+}

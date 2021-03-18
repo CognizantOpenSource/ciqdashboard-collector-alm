c72f8a409d8cb8cfe9fff79dae81a2c223dd0263
Shankar Mahadevan <Shankar.Mahadevan2@cognizant.com>
2021-03-18 15:56:42 +0530

    License and author update
    License and author update in all java files

--- src/main/java/com/cognizant/collector/alm/config/JsonUtil.java
+++ src/main/java/com/cognizant/collector/alm/config/JsonUtil.java
@@ -1,3 +1,20 @@
+
+/*
+ *    © [2021] Cognizant. All rights reserved.
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http:www.apache.orglicensesLICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
 package com.cognizant.collector.alm.config;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -13,8 +30,10 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by 784420 on 7/3/2019 10:36 AM
+/*
+ * JsonUtil
+ *
+ * @author Cognizant
  */
 public class JsonUtil {
 
